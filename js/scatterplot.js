6716f49609b7756c83ebd300cca4aaf251963a52
occhipintif <faith.occhipinti@comcast.net>
2022-02-28 13:18:45 -0500

    update

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -8,11 +8,6 @@ Modified: 12/08/21
 
 // Build your scatterplot in this file 
 
-const width = 900; 
-const height = 450; 
-const margin = {left:50, right:50, bottom:50, top:50}; 
-const yTooltipOffset = 15; 
-
 const svg3 = d3
   .select("#csv-scatter")
   .append("svg")
@@ -21,30 +16,24 @@ const svg3 = d3
   .attr("viewBox", [0, 0, width, height]);
 
 d3.csv("data/scatter.csv").then((data3) => {
-  
-  let x1, y1, x2, y2, x3, y3;  
-
-  let xKey1, yKey1, xKey2, yKey2, xKey3, yKey3;
-
   {
-    let xKey1 = "day";
-    let yKey1 = "score";
+    xKey1 = "day";
+    yKey1 = "score";
 
     // Find max x
-    let maxX1 = d3.max(data3, (d) => { return d[xKey1]; });
+    maxX1 = d3.max(data3, (d) => { return d[xKey1]; });
 
     maxY1 = d3.max(data3, function(d) { return d.score; });
 
-    // Create X scale
-    let x1 = d3.scaleLinear()
+    x1 = d3.scaleLinear()
                 .domain([0,maxX1])
                 .range([margin.left, width-margin.right]); 
 
-    let yScale1 = d3.scaleLinear()
+    yScale1 = d3.scaleLinear()
             .domain([0,maxY1])
             .range([height-margin.bottom,margin.top]); 
 
-    let xScale1 = d3.scaleBand()
+    xScale1 = d3.scaleBand()
             .domain(d3.range(data3.length))
             .range([margin.left, width - margin.right])
             .padding(0.1); 
@@ -80,15 +69,14 @@ const mouseleave3 = function(event, d) {
   tooltip3.style("opacity", 0); 
 }
 
-svg3.selectAll(".bar") 
+svg3.selectAll(".scatter") 
    .data(data3) 
    .enter()  
    .append("circle") 
      .attr("class", "scatter") 
-     .attr("x", (d,i) => xScale1(i)) 
-     .attr("y", (d) => yScale1(d.score)) 
-     .attr("height", (d) => (height - margin.bottom) - yScale1(d.score)) 
-     .attr("width", xScale1.bandwidth()) 
+     .attr("cx", (d,i) => xScale1(i)) 
+     .attr("cy", (d) => yScale1(d.score))
+     .attr("r", 10) 
      .on("mouseover", mouseover3) 
      .on("mousemove", mousemove3)
      .on("mouseleave", mouseleave3);
